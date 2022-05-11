87171c30595f91ad55d83b8ef2b791e84c011561
yemmyharry <yemmyharry@gmail.com>
2022-05-11 11:06:43 +0100

    Add cors configuration

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"restAuth/database"
 	"restAuth/routes"
 )
@@ -9,6 +10,8 @@ import (
 func main() {
 	database.Connect()
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{AllowCredentials: true}))
 
 	routes.Setup(app)
 
