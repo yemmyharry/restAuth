24fd73a673d35dcaa15cea32e7bbecd61bb77f8e
yemmyharry <yemmyharry@gmail.com>
2022-05-11 11:07:21 +0100

    Add login controller

--- controllers/authController.go
+++ controllers/authController.go
@@ -2,9 +2,11 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 	"restAuth/database"
 	"restAuth/models"
+	"time"
 )
 
 func Hello(c *fiber.Ctx) error {
@@ -40,4 +42,62 @@ func Register(c *fiber.Ctx) error {
 		"status": "success",
 		"user":   user,
 	})
+}
+
+func Login(c *fiber.Ctx) error {
+
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return c.JSON(fiber.Map{
+			"status": err.Error(),
+		})
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user)
+
+	if user.ID == 0 {
+		c.Status(fiber.StatusNotFound)
+		return c.JSON(fiber.Map{
+			"status": "User not found",
+		})
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(data["password"]))
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"status": "Wrong password",
+		})
+	}
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+		Issuer:    string(user.ID),
+	})
+
+	token, err := claims.SignedString([]byte("secret"))
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"status": "Error signing token",
+		})
+	}
+
+	cookie := &fiber.Cookie{
+		Name:     "jwt-token",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+
+	c.Cookie(cookie)
+
+	return c.JSON(fiber.Map{
+		"status": "logged in",
+		"user":   user,
+	})
+
 }
