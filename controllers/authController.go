83fbe9a56a53f78f0a50cf171aa96d62dc5a1752
yemmyharry <yemmyharry@gmail.com>
2022-05-11 10:04:05 +0100

    Use bcrypt to hash password

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,6 +1,11 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+	"restAuth/database"
+	"restAuth/models"
+)
 
 func Hello(c *fiber.Ctx) error {
 	return c.SendString("Hello, World 👋!")
@@ -8,14 +13,31 @@ func Hello(c *fiber.Ctx) error {
 
 func Register(c *fiber.Ctx) error {
 
-	var data map[string]interface{}
+	var data map[string]string
 	if err := c.BodyParser(&data); err != nil {
 		return c.JSON(fiber.Map{
 			"status": "error",
 		})
 	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(data["password"]), bcrypt.DefaultCost)
+	if err != nil {
+		return c.JSON(fiber.Map{
+			"status": "Error hashing password",
+			"error":  err.Error(),
+		})
+	}
+
+	user := models.User{
+		Name:     data["name"],
+		Email:    data["email"],
+		Password: string(hashedPassword),
+	}
+
+	database.DB.Create(&user)
+
 	return c.JSON(fiber.Map{
 		"status": "success",
-		"user":   data,
+		"user":   user,
 	})
 }
