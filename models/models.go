a8c0804bef3841ac6aea47aece2aee8f7060acde
yemmyharry <yemmyharry@gmail.com>
2022-05-16 19:43:24 +0100

    Add password reset struct

--- models/models.go
+++ models/models.go
@@ -6,5 +6,11 @@ type User struct {
 	gorm.Model
 	Name     string `json:"name" gorm:"type:varchar(32) not null"`
 	Email    string `json:"email" gorm:"type:varchar(32) unique not null"`
-	Password string `json:"password" gorm:"type:varchar(256) not null"`
+	Password string `json:"-" gorm:"type:varchar(256) not null"`
+}
+
+type PasswordReset struct {
+	Id    uint   `gorm:"primary_key" json:"id"`
+	Email string `gorm:"type:varchar(32) unique not null" json:"email"`
+	Token string `gorm:"type:varchar(256) not null" json:"token"`
 }
